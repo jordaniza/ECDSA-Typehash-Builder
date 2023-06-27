26236286ee3d5487b3a8e6a724fb1f780054ede8
jordan <jordaniza@proton.me>
2023-06-27 14:03:21 +0200

    floating pragma

--- src/HashBuilder.sol
+++ src/HashBuilder.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.16;
+pragma solidity ^0.8.0;
 
 import {ECDSA} from "@oz/utils/cryptography/ECDSA.sol";
 import {IERC20Permit} from "@oz/token/ERC20/extensions/draft-IERC20Permit.sol";
